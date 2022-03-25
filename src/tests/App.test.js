9143f84abcec6f004e1b2c40a310c909f9945af8
ivanlod3 <ivanlod3@gmail.com>
2022-03-25 01:02:20 +0100

    Fix test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -14,10 +14,10 @@ describe('<App />', () => {
     expect(startButton).toBeInTheDocument();
   });
 
-  // TODO redirection test
-  // test('redirects to Game component when loggedIn', () => {
-  //   localStorage.setItem('currentPlayer', 'test');
-  //   const header = component.getByText('User:');
-  //   expect(header).toBeInTheDocument();
-  // });
+  test('redirects to Game component when click start button', () => {
+    const startButton = component.getByText('Start');
+    startButton.click();
+    const header = component.getByText('User:');
+    expect(header).toBeInTheDocument();
+  });
 });
